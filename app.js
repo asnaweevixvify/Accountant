99c01c5ca0d57c32a668a0f437a389cf2e63e335
asnaweevixvify <asnaweeezor@gmail.com>
2025-04-04 15:10:57 +0700

    system

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
-
+let  moneyValuefinal1 = 0;
+let  moneyValuefinal2 = 0;
+let  totalMoneyValuefinal = 0;
 function addToHistory(){
     let bankName = document.getElementById('bankname').value;
     let moneyValue = document.getElementById('moneyvalue').value;
@@ -7,6 +9,9 @@ function addToHistory(){
     let addItems = document.createElement('li')
     let earnText = document.getElementById('earntext');
     let payText = document.getElementById('paytext');
+    let totalMoneyValue = document.getElementById('totalmoney');
+    
+    
     if(moneyValue>0){
         addItems.innerHTML=`
         <li class="animate__animated animate__bounceIn" id="historyList">
@@ -14,10 +19,25 @@ function addToHistory(){
             <h3 class="historyListText">฿${moneyValue}</h3>
             <p class="line-3"></p>
         </li>
-    `;
+    `;  
+        moneyValuefinal1 += parseFloat(moneyValue) 
+        earnText.innerHTML=`
+        ฿${moneyValuefinal1}
+
+    `;  
+        totalMoneyValuefinal += parseFloat(moneyValue) 
+        totalMoneyValue.innerHTML=`
+        ฿${totalMoneyValuefinal};
+    `;  
+        
         historyAll.appendChild(addItems);
+        Swal.fire({
+            title: "เพิ่มรายการสำเร็จ",
+            icon: "success",
+            draggable: true
+    });      
     }
-    else{
+    else if (moneyValue<0){
         addItems.innerHTML=`
         <li class="animate__animated animate__bounceIn" id="historyList" >
             <h3 class="historyListText">${bankName}</h3>
@@ -26,6 +46,33 @@ function addToHistory(){
         </li>
     `;
         historyAll.appendChild(addItems);
-        payText += moneyValue
+        Swal.fire({
+            title: "เพิ่มรายการสำเร็จ",
+            icon: "success",
+            draggable: true
+    });  
+        moneyValuefinal2 += parseFloat(moneyValue) 
+        payText.innerHTML=`
+        ฿${-moneyValuefinal2}
+    `;  
+        totalMoneyValuefinal += parseFloat(moneyValue); 
+        totalMoneyValue.innerHTML=`
+        ฿${totalMoneyValuefinal}
+    `;  
     }
+    else if(bankName == ""){
+        Swal.fire({
+            title: "กรุณาระบุชื่อ",
+            icon: "error",
+            draggable: true
+          });
+    }
+    else if(moneyValue == ""){
+        Swal.fire({
+            title: "กรุณาระบุจำนวนเงิน",
+            icon: "error",
+            draggable: true
+          });
+    }
+    
 }
